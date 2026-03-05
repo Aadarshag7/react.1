4e87232d6fa85afec0fad4c5d046b4b6a0ec49e7
Aadarshag7 <aadarshgautam40@gmail.com>
2026-03-05 23:04:09 +0545

    react 2

--- education-ui/src/Counter.jsx
+++ education-ui/src/Counter.jsx
@@ -5,20 +5,30 @@ import { useRef } from "react";
 function Counter(){
 const[count,setCount]=useState(0);
 const intervalRef=useRef(null);
-useEffect(()=>{
+const startCounter=()=>{
+    if(intervalRef.current)return;
+
+
      intervalRef.current =setInterval(()=>{
         setCount(prev=>prev+1);
     },1000);
-    return ()=>clearInterval(intervalRef.current);
-},[]);
+};
 const stopCounter=()=>{
     clearInterval(intervalRef.current);
+};
+
+const resetCounter=()=>{
+    clearInterval(intervalRef.current);
+    intervalRef.current=null;
+    setCount(0);
 };
 
 return(
     <div>
      <h1>{count}</h1>
      <button onClick={stopCounter} >Stop</button>
+     <button onClick={startCounter}>Start</button>
+     <button onClick={resetCounter}>Reset</button>
   </div>);
 
 }
