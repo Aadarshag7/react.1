cac4d94410e91d09f5ff63f113e756ea78917a37
Aadarshag7 <aadarshgautam40@gmail.com>
2026-03-11 22:35:44 +0545

    react filter

--- education-ui/src/Users.jsx
+++ education-ui/src/Users.jsx
@@ -3,6 +3,7 @@ import { useState,useEffect } from "react";
 export default function Users(){
     const[users,setUser]=useState([]);
     const[loading,setLoading]=useState(true);
+    const[search,setSearch]=useState("");
 useEffect(()=>{
     const fetchUser=async()=>{
         try{
@@ -14,11 +15,15 @@ useEffect(()=>{
         } finally{ 
             setLoading(false);
         }
+   
+
         };
 
 
     fetchUser();
 },[]);
+   const filterUser=users.filter((user)=>
+         user.name.toLowerCase().includes(search.toLowerCase()));
 
 if (loading){
     return (<h2>Loading..</h2>);
@@ -26,14 +31,24 @@ if (loading){
 
 return(
     <div>
-        {users.map((user)=>(
+        <input type="text" value={search} placeholder="Search"
+        onChange={(e)=>setSearch(e.target.value)}
+        />
+
+        {filterUser.map((user)=>(
             <div key={user.id}>
                 <h2>{user.name}</h2>
                 <p>{user.email}</p>
             </div>
 
         ))}
+
+        
+
+
     </div>
+
+
 );
 
 }
