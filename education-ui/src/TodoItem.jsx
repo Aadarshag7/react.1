3cbd9f3fb9f8649b14c98f22013c26508b6c8bd3
Aadarshag7 <aadarshgautam40@gmail.com>
2026-03-23 21:22:47 +0545

    react destructuring

--- education-ui/src/TodoItem.jsx
+++ education-ui/src/TodoItem.jsx
@@ -1,5 +1,14 @@
-export default function TodoItem(todo,index,deletes,toggle){
+export default function TodoItem({todo,index,deletes,toggle}){
 return(
-    <li 
-)
+    <li
+     onClick={()=>toggle(index)}
+    style={{textDecoration:todo.completed?
+        "line-through":"none",
+        cursor:"pointer"
+    }}
+>    
+    {todo.text}
+    <button onClick={()=>delete(index)}>Delete</button>
+    </li>
+);
 }
