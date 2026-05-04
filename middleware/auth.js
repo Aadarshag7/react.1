778f4305c1a5d2ddbf4df105ba52e0e8e61e54b2
Aadarshag7 <aadarshgautam40@gmail.com>
2026-05-04 21:08:54 +0545

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,15 @@
-ds
+const jwt=require("jsonwebtoken");
+const auth=(req,res,next)=>{
+    try{
+    const tokens=req.cookies.token;
+    if(!tokens){
+        return res.status(401).json({
+            message:"Not authorized"
+        });
+    }
+    const decoded=jwt.verify(tokens,"aadarshag");
+    req.user=decoded;
+    next();
+    }
+
+}
